4e6aec6c5691fa469d504217b2428a89c23716b6
Michael Kelly <michael@nextcommerce.com.au>
2014-12-31 17:51:11 +1100

    renamed elp->wviews, added symbolic link to dlv

--- wviews.py
+++ wviews.py
@@ -75,6 +75,37 @@ class Atom(object):
         return hash(self.__str__())
 
 
+class Rule(object):
+    def __init__(self, head, tail):
+        self.head = head
+        self.tail = tail
+
+    def parse_rule_string(self, rule_string):
+
+        body = rule_string.split(':-')
+        if len(body) == 1:  # atom or disjunctive atom
+            self.head = {token.strip() for token in body[0].strip().split(' v ')}
+        elif len(body) == 2:  # rule or constraint
+            self.head = {token.strip().replace(' ', '') for token in body[0].split(' v ')}
+            self.tail = set()
+            for token in body[1].split(','):
+                if token.strip().startswith('not '):
+                    token = token.replace(' ', '')
+                self.tail.add(token.strip())
+        return self
+
+    def get_rule_head_string(self):
+        return ' v '.join([str(atom) for atom in self.head])
+
+    def get_rule_tail_string(self):
+        return ', '.join([str(atom) for atom in self.tail])
+
+    def get_rule_string(self):
+        if self.tail:
+            return '%s :- %s.' % (self.get_rule_head_string(), self.get_rule_tail_string())
+        return '%s.' % self.get_rule_head_string()
+
+
 class WorldViews(object):
     """
     **************************************************************************
