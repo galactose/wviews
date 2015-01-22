3cfd2486b9da9ea72f80a825eb303b4bf8639234
galactose <mkelly.deft@gmail.com>
2015-01-22 22:19:25 +1100

    more comments

--- program/rule.py
+++ program/rule.py
@@ -5,11 +5,24 @@ class BrokenConstraint(Exception):
 
 class Rule(object):
     def __init__(self, rule_string):
+        """
+        An object loosely representing a rule in a logic program,
+
+        Arguments:
+         * rule_string (str) - a rule that follows the syntax of an epistemic logic program rule.
+        """
         self.head = set()
         self.tail = set()
         self.parse_rule_string(rule_string)
 
     def parse_rule_string(self, rule_string):
+        """
+        Given a string following the syntax of an epistemic logic program break it down into sections usable in
+        evaluating it's valid worldviews.
+
+        Arguments:
+         * rule_string (str) - a rule that follows the syntax of an epistemic logic program rule.
+        """
         body = rule_string.split(':-')
         if len(body) == 1:  # atom or disjunctive atom
             self.head = {token.strip() for token in body[0].strip().split(' v ')}
@@ -41,6 +54,13 @@ class IndexedRule(object):
         return ' v '.join([str(self.atom_dict[atom]) for atom in self.head])
 
     def get_rule_tail_string(self, apply_valuation=False):
+        """
+        For the rule, run through the indexed atoms and return the representative string to be sent to dlv.
+        If apply_valuation is true apply the semantics of an evaluated rule.
+
+        Arguments:
+         * apply_valuation (bool) - apply epistemic atom semantics to the rule based on the valuations.
+        """
         if apply_valuation:
             rule_tail = []
             for atom_id in self.tail:
